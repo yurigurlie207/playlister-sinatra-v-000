e255b8876e6a3e2e9b5d28e1884da3d2c606d520
Adrienne Yao <adrienne.yao@icloud.com>
2020-02-11 20:30:27 +0000

    Automatically backed up by Learn

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -1,13 +1,13 @@
-module Slugifiable
-  module InstanceMethods
-    def slug
-      self.name.gsub(" ", "-").downcase
-    end
-  end
-
-  module ClassMethods
-    def find_by_slug(slug)
-      self.all.find{ |instance| instance.slug == slug }
-    end
-  end
-end
+# module Slugifiable
+#   module InstanceMethods
+#     def slug
+#       self.name.gsub(" ", "-").downcase
+#     end
+#   end
+#
+#   module ClassMethods
+#     def find_by_slug(slug)
+#       self.all.find{ |instance| instance.slug == slug }
+#     end
+#   end
+# end
